704638166d797d273e9e0bc39ac37f2b8b62935a
Dylan <dylandixon@hotmail.com>
2021-05-03 20:35:56 -0230

    Add logic for if number is greater than 25

--- caesar-cipher/main.rb
+++ caesar-cipher/main.rb
@@ -1,7 +1,6 @@
+require 'pry'
+
 def caesar_cipher(string, shift)
-  if shift >= 25
-    return "Please enter a number lower than 25."
-  else
     string_characters = string.bytes
     shifted_chars = string_characters.map do |character|
       unless ((character >= 65 && character <= 90) || (character >= 97 && character <= 122))
@@ -12,23 +11,32 @@ def caesar_cipher(string, shift)
     end
     final_chars = shifted_chars.map {|char| char.chr}.join('')
     return final_chars
-  end
 end
 
 def shifter(character, shift)
-  if character >= 97
-    if (character + shift) > 122
-      shift -= 122 - character
-      return 97 + shift
-    else
-      return character + shift
-    end
+  if shift < 0
+    
   else
-    if (character + shift) > 90
-      shift -= 90 - character
-      return 65 + shift
+    if shift > 25
+      shift = shift % 25
+    end
+
+    if character >= 97
+      if (character + shift) > 122
+        shift -= 122 - character
+        return 97 + shift
+      else
+        return character + shift
+      end
     else
-      return character + shift
+      if (character + shift) > 90
+        shift -= 90 - character
+        return 65 + shift
+      else
+        return character + shift
+      end
     end
   end
 end
+
+p caesar_cipher("Amongnugn", 52)

fda69c764041a465803724e013a92006a16e3b03
Dylan <dylandixon@hotmail.com>
2021-05-03 22:58:16 -0230

    Reworked logic to make more sense, now takes any input, positive or negative, and works.

--- caesar-cipher/main.rb
+++ caesar-cipher/main.rb
@@ -1,42 +1,32 @@
-require 'pry'
-
 def caesar_cipher(string, shift)
-    string_characters = string.bytes
-    shifted_chars = string_characters.map do |character|
-      unless ((character >= 65 && character <= 90) || (character >= 97 && character <= 122))
-        character
-      else
-        shifter(character, shift)
-      end
-    end
-    final_chars = shifted_chars.map {|char| char.chr}.join('')
-    return final_chars
-end
-
-def shifter(character, shift)
-  if shift < 0
-    
-  else
-    if shift > 25
-      shift = shift % 25
-    end
-
-    if character >= 97
-      if (character + shift) > 122
-        shift -= 122 - character
-        return 97 + shift
-      else
-        return character + shift
-      end
+  string_characters = string.bytes
+  string_characters.map do |char|
+    shift_counter = shift 
+    unless ((char >= 65 && char <= 90) || (char >= 97 && char <= 122))
+      char
     else
-      if (character + shift) > 90
-        shift -= 90 - character
-        return 65 + shift
-      else
-        return character + shift
+      until shift_counter == 0
+        if shift_counter < 0
+          unless (char == 65) || (char == 97)
+            shift_counter += 1
+            char -= 1
+          else
+            shift_counter += 1
+            char += 25
+          end
+        else
+          unless (char == 122) || (char == 90)
+            shift_counter -= 1
+            char += 1
+          else
+            shift_counter -= 1
+            char -= 25
+          end
+        end
       end
     end
+    char.chr
   end
 end
 
-p caesar_cipher("Amongnugn", 52)
+p caesar_cipher("A z Z a", -51)

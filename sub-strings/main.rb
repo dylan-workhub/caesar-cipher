fec5319492debc4c7d0ba50125a7760d0a731218
Dylan <dylandixon@hotmail.com>
2021-05-04 14:51:24 -0230

    Complete logic for finding substring in string

--- sub-strings/main.rb
+++ sub-strings/main.rb
@@ -1,1 +1,19 @@
+require 'pry'
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def sub_strings(string, dictionary)
+  string = string.downcase
+  dictionary_hash = dictionary.reduce(Hash.new(0)) do |result, word|
+    if string.include?(word)
+      string_total = string.scan(word)
+      result[word] += string_total.length
+      result
+    else
+      result
+    end
+  end
+  return dictionary_hash
+end
+
+p sub_strings("siterator it lowlow", dictionary)
